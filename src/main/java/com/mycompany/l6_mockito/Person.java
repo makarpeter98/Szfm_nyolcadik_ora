e3f1f294244fa7515cbdb43b2db377e4c5f8463b
makar <makar.peter98@gmail.com>
2025-11-11 16:21:50 +0100

    Person - equals

--- src/main/java/com/mycompany/l6_mockito/Person.java
+++ src/main/java/com/mycompany/l6_mockito/Person.java
@@ -36,6 +36,22 @@ public class Person {
 
     @Override
     public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Person other = (Person) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
         return true;
     }
 
